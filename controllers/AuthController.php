11723527cc82700334ff222cf6763d136d238344
cavardloris <cavardloris@gmail.com>
2025-12-11 14:17:28 +0100

    UserManager terminé, register.html.twig terminé et AuthController.php commencé

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,4 +6,54 @@
         {
             $this->render('home/home.html.twig', []);
         }
+
+        public function logout() : void
+        {
+            session_destroy();
+            $this->redirect('index.php');
+        }
+
+        public function register() : void
+        {
+            $error = [];
+
+            if(!empty($_POST))
+            {
+                if((empty($_POST["name"])) || (empty($_POST["email"])) || (empty($_POST["password"])) || (empty($_POST["confirmPassword"])) )
+                {
+                    $error[] = "Veuillez remplir tous les champs";
+                }
+                else
+                {
+                    $ctrl = new UserManager;
+                    $email = $ctrl->findByEmail($_POST["email"]);
+                    if($email !== null)
+                    {
+                        $error[] = "Cet utilisateur existe déjà !";
+                        $this->render('auth/register.html.twig', ["errors" => $error]);
+                    }
+                    else
+                    {
+                        if($_POST["password"] !== $_POST["confirmPassword"])
+                        {
+                            $error[] = "Les mots de passe ne correspondent pas !";
+                            $this->render('auth/register.html.twig', ["errors" => $error]);
+                        }
+                        else
+                        {
+                                $hashedPassword = password_hash($_POST["password"], PASSWORD_DEFAULT);
+                                $ctrl = new UserManager;
+                                $user = new User($_POST["name"], $_POST["email"], $hashedPassword);
+                                $ctrl->create($user);
+                                $this->redirect("index.php");
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                $this->render('auth/register.html.twig', ["errors" => $error]);
+            }
+        }
     }
---
+++ managers/UserManager.php
@@ -0,0 +1,94 @@
+<?php
+
+class UserManager extends AbstractManager
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function findAll() : array
+    {
+        $query = $this->db->prepare('SELECT * FROM users');
+        $parameters = [
+
+        ];
+        $query->execute($parameters);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $users = [];
+
+        foreach($result as $item)
+        {
+            $user = new User($item["name"], $item["email"], $item["password"], $item["id"]);
+            $users[] = $user;
+        }
+
+        return $users;
+    }
+
+    public function findById(int $id) : ? User
+    {
+        $query = $this->db->prepare('SELECT * FROM users WHERE id = :id');
+        $parameters = [
+            "id" => $id
+        ];
+        $query->execute($parameters);
+        $item = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($item)
+        {
+            return new User($item["name"], $item["email"], $item["password"], $item["id"]);
+        }
+
+        return null;
+    }
+
+    public function findByEmail(string $email) : ? User
+    {
+        $query = $this->db->prepare('SELECT * FROM users WHERE email = :email');
+        $parameters = [
+            "email" => $email
+        ];
+        $query->execute($parameters);
+        $item = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($item)
+        {
+            return new User($item["name"], $item["email"], $item["password"], $item["id"]);
+        }
+
+        return null;
+    }
+
+    public function create(User $user) : void
+    {
+        $query = $this->db->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');;
+        $parameters = [
+            "name" => $user->getName(),
+            "email" => $user->getEmail(),
+            "password" => $user->getPassword()
+        ];
+        $query->execute($parameters);
+    }
+
+    public function update(User $user) : void
+    {
+        $query = $this->db->prepare('UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id');;
+        $parameters = [
+            "id" => $user->getId(),
+            "name" => $user->getName(),
+            "email" => $user->getEmail(),
+            "password" => $user->getPassword()
+        ];
+        $query->execute($parameters);
+    }
+
+    public function delete(User $user) : void
+    {
+        $query = $this->db->prepare('DELETE FROM users WHERE id = :id');;
+        $parameters = [
+            "id" => $user->getId()
+        ];
+        $query->execute($parameters);
+    }
+}
