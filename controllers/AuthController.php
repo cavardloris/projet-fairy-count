7ff85f8d527ecd42d2f6259d3589aae7a6957d36
cavardloris <cavardloris@gmail.com>
2025-12-11 14:30:18 +0100

    creation de la page notFound.html.twig et ajout de la fonction notFound dans AuthController

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -56,4 +56,52 @@
                 $this->render('auth/register.html.twig', ["errors" => $error]);
             }
         }
+
+        public function login() : void
+        {
+            $error = [];
+
+            if(!empty($_POST))
+            {
+                if((empty($_POST["email"])) || (empty($_POST["password"])))
+                {
+                    $error[] = "Veuillez remplir tous les champs";
+                }
+                else{
+                    $ctrl = new UserManager;
+                    $user = $ctrl->findByEmail($_POST["email"]);
+                    if($user === null)
+                    {
+                        $error[] = "Identifiants invalides";
+                        $this->render('auth/login.html.twig', ["errors" => $error]);
+                    }
+                    else
+                    {
+                        $mdp = $user->getPassword();
+                        if(password_verify($_POST["password"], $mdp))
+                        {
+                            $_SESSION["name"] = $user->getName();
+                            $_SESSION["email"] = $user->getEmail();
+                            $_SESSION["id"] = $user->getId();
+                            $this->redirect('index.php?route=profile'); 
+                        }
+                        else
+                        {
+                            $error[] = "Identifiants invalides";
+                            $this->render('auth/login.html.twig', ["errors" => $error]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                $this->render('auth/login.html.twig', []);
+            }
+            
+        }
+
+        public function notFound():void 
+        {
+            $this->render('error/notFound.html.twig');
+        }
     }
